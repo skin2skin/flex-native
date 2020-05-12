ac6169d074d43a9a32f7eb460c1a68f7b1615b7b
xiamin <xiamin@tenda.cn>
2020-05-12 18:33:31 +0800

    1

--- webpack.dev.js
+++ webpack.dev.js
@@ -31,7 +31,7 @@ module.exports = {
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            template: './test/index.html',
+            template: './test/index2.html',
             filename: 'index.html',
             hash: true,
             minify: {
